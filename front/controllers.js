a6b93b5bc961fc6d0956dead781d25f34b4c9fd5
Matthias-Kelvin Daous <mkdaous@gmail.com>
2016-06-02 23:59:23 +0200

    install player

--- front/controllers.js
+++ front/controllers.js
@@ -4,9 +4,9 @@ angular
     loadSongs();
 
     $scope.$on('remove', function(event, song) {
-        $rootScope.songs.removeItemById(song);
 
         $http.delete("/songs/"+song._id).success(function(data) {
+            $rootScope.songs.removeItemById(song);
         });
     });
 
@@ -23,6 +23,19 @@ angular
         $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
         $scope.predicate = predicate;
     };
+})
+.controller('PlayerController', function($rootScope, $http) {
+
+    if ($rootScope.songs == undefined) {
+        $http.get("/songs").success(function(data) {
+            $rootScope.songs = data;
+            loadPlayer($rootScope.songs);
+
+        });
+    }
+    else {
+        loadPlayer($rootScope.songs);
+    }
 })
 .controller("EditSongController", function($scope, $rootScope, $routeParams, $http, $location) {
     var songId = $routeParams['id']
@@ -55,3 +68,97 @@ angular
     });
 });
 
+
+
+function loadPlayer(songs) {
+
+    var b = document.documentElement;
+
+    b.setAttribute('data-useragent', navigator.userAgent);
+    b.setAttribute('data-platform', navigator.platform);
+
+    var tracks = []
+
+    for (i in songs) {
+        tracks.push({
+            "track": 1,
+            "name": songs[i].title,
+            "length": songs[i].duration,
+            "file": songs[i].album+'/'+songs[i].name
+        })
+    }
+
+    var supportsAudio = !!document.createElement('audio').canPlayType;
+    if (supportsAudio) {
+        var index = 0,
+            playing = false,
+            mediaPath = 'http://localhost/node/song_api/albums/',
+            extension = '',
+            tracks = tracks,
+            trackCount = tracks.length,
+            npAction = $('#npAction'),
+            npTitle = $('#npTitle'),
+            audio = $('#audio1').bind('play', function () {
+                playing = true;
+                npAction.text('Now Playing...');
+            }).bind('pause', function () {
+                playing = false;
+                npAction.text('Paused...');
+            }).bind('ended', function () {
+                npAction.text('Paused...');
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    audio.play();
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }).get(0),
+            btnPrev = $('#btnPrev').click(function () {
+                if ((index - 1) > -1) {
+                    index--;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            btnNext = $('#btnNext').click(function () {
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            li = $('#plList li').click(function () {
+                var id = parseInt($(this).index());
+                if (id !== index) {
+                    playTrack(id);
+                }
+            }),
+            loadTrack = function (id) {
+                $('.plSel').removeClass('plSel');
+                $('#plList li:eq(' + id + ')').addClass('plSel');
+                npTitle.text(tracks[id].name);
+                index = id;
+                audio.src = mediaPath + tracks[id].file;
+            },
+            playTrack = function (id) {
+                loadTrack(id);
+                audio.play();
+            };
+        loadTrack(index);
+    }
+}
