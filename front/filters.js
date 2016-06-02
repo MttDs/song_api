b99fa93e79d5571ad17905c3369a2406291c3dbc
unknown <saranjan@outlook.com>
2016-06-02 23:26:45 +0200

    modification tuto + modif template edition

--- front/filters.js
+++ front/filters.js
@@ -2,8 +2,9 @@ angular
 .module("Filters", [])
 .filter("sec2min", function(){
    return function(sec){
+    console.log(sec);
         var minutes = Math.floor(sec / 60);
-        var reste = sec - minutes * 60;
+        var reste = sec - (minutes * 60);
 
         return minutes + "mn "+ reste + "s";
     }
